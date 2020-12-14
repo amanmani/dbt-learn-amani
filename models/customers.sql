30efb2e1e99b86c3da0beff33bd59325191bc26b
Aman Mani <aman.mani@jetblue.com>
2020-12-14 03:47:16 +0000

    Add customers model

---
+++ models/customers.sql
@@ -0,0 +1,41 @@
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+
+),
+
+orders as (
+
+ select * from {{ ref('stg_orders')}}
+
+),
+
+customer_orders as (
+
+select * from {{ ref ('stg_customers')}}
+
+),
+
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
+)
+
+select * from final
